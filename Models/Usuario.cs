dece978a121df3282824db7c1f229628f4e661b4
Jacofsky <valentinjacofskyort@gmail.com>
2022-04-04 09:52:09 -0300

    Usuario model

---
+++ Models/Usuario.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace TP_1_DAI.Models
+{
+    public class Usuario
+    {
+        public int Id {get; set;}
+        public string Nombre {get; set;}
+        public string Apellido {get; set;}
+        public string UserName {get; set;}
+        public string Password {get; set;}
+        public string Token {get; set;}
+        public DateTime TokenExpirationDate {get; set;}
+
+    }
+
+}
